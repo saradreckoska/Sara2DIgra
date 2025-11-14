d0d20a7ec4fd01050f460a5771c80bd5077eda67
saradreckoska <saradreckoska22@gmail.com>
2025-11-14 13:02:47 +0100

    knight enemy

---
+++ Assets/scripts/Traps1/ArrowTrap.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class ArrowTrap : MonoBehaviour
+{
+    [SerializeField] private float attackCooldown;
+    [SerializeField] private Transform firePoint;
+    [SerializeField] private GameObject[] arrows;
+    private float cooldownTimer;
+
+    private void Attack()
+{
+    cooldownTimer = 0;
+
+    int arrowIndex = FindArrow();
+    GameObject arrow = arrows[arrowIndex];
+
+    arrow.transform.position = firePoint.position;
+    arrow.GetComponent<EnemyProjectile>().ActivateProjectile();
+}
+
+    private int FindArrow()
+    {
+        for (int i = 0; i < arrows.Length; i++)
+        {
+            if (!arrows[i].activeInHierarchy)
+                return i;
+        }
+        return 0;
+    }
+    private void Update()
+    {
+        cooldownTimer += Time.deltaTime;
+
+        if (cooldownTimer >= attackCooldown)
+            Attack();
+    }
+}
